454f7109f405fe4c03be97f54d4287b5e4aa0fe8
nssteinbrenner <spark@desktop-01.flylikedaedalus.com>
2020-08-23 04:49:58 -0700

    test errors

--- anitogo_test.go
+++ anitogo_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -161,6 +162,41 @@ func TestParse(t *testing.T) {
 			t.Error(err)
 		}
 		t.Fatalf("Failed %d/%d cases\n%s", len(notMatched), len(e), notMatchedJSON)
+	}
+}
+
+func TestTraceError(t *testing.T) {
+	err := traceError(indexTooLargeErr)
+	if err == nil {
+		t.Error("expected error got nil")
+	} else {
+		if strings.Index(err.Error(), indexTooLargeErr) == -1 {
+			t.Errorf("expected %s in error, got %s", indexTooLargeErr, err.Error())
+		}
+	}
+	err = traceError(indexTooSmallErr)
+	if err == nil {
+		t.Error("expected error got nil")
+	} else {
+		if strings.Index(err.Error(), indexTooSmallErr) == -1 {
+			t.Errorf("expected %s in error, got %s", indexTooSmallErr, err.Error())
+		}
+	}
+	err = traceError(endIndexTooSmallErr)
+	if err == nil {
+		t.Error("expected error got nil")
+	} else {
+		if strings.Index(err.Error(), endIndexTooSmallErr) == -1 {
+			t.Errorf("expected %s in error, got %s", endIndexTooSmallErr, err.Error())
+		}
+	}
+	err = traceError(tokensEmptyErr)
+	if err == nil {
+		t.Error("expected error got nil")
+	} else {
+		if strings.Index(err.Error(), tokensEmptyErr) == -1 {
+			t.Errorf("expected %s in error, got %s", tokensEmptyErr, err.Error())
+		}
 	}
 }
 
